888e16634b35082b5245842612612b3bb9384699
root <root@vultr.guest>
2020-09-09 13:21:57 +0000

    update

---
+++ include/my_tree_with_arr.hpp
@@ -0,0 +1,11 @@
+#ifndef MY_TREE_WITH_ARR_
+#define MY_TREE_WITH_ARR_
+#include <iostream>
+#ifndef MAXN
+#define MAXN 1000
+#endif // MAXN
+
+struct MyTreeNode {
+    int val, level;
+};
+#endif // MY_TREE_WITH_ARR_
